48560381fea25e1a769e615ff9a08e62c5531402
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-03-24 04:08:29 -0400

    Set up basic components in scr folder with functions, not replacting on simulator, will check later

---
+++ src/screens/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import styles from './style';
+
+const HomeScreen = props => {
+  return (
+    <View>
+      <Text>Hi</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
