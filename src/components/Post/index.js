d39b02614d2797a18465df526eba0b4561a888ca
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-03-25 15:06:44 -0400

    Images with styles, also redirected image source directly from Airbnb site for educational purposes only

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -4,8 +4,22 @@ import styles from './styles.js';
 
 const Post = props => {
   return (
-    <View>
-      <Text>Post components</Text>
+    <View style={styles.container}>
+      {/* Image */}
+      <Image
+        style={styles.image}
+        source={{
+          uri:
+            'https://a0.muscache.com/im/pictures/miso/Hosting-36908284/original/30fb92c2-e4df-479d-912a-c63de33415e0.jpeg?im_w=480',
+        }}
+      />
+      {/* Bed and Bedroom */}
+
+      {/* Type description */}
+
+      {/* Old and New price */}
+
+      {/* Total price */}
     </View>
   );
 };
