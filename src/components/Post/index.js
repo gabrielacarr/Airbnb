64bc1039758d5ded90b4080af37919c6af62f3bc
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-03-25 14:55:22 -0400

    Post component index and style implemented structure and text appears on simulators

---
+++ src/components/Post/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text, Image, Pressable} from 'react-native';
+import styles from './styles.js';
+
+const Post = props => {
+  return (
+    <View>
+      <Text>Post components</Text>
+    </View>
+  );
+};
+
+export default Post;
