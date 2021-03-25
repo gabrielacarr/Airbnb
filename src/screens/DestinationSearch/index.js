21887202b994682449cbda7915df9025863205fd
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-03-25 18:25:18 -0400

    Was unable to figure out why everything was broken, of course a typo while rendering, issue resolved: dummy search reflects correctly, looking good

--- src/screens/DestinationSearch/index.js
+++ src/screens/DestinationSearch/index.js
@@ -1,16 +1,34 @@
-import React from 'react';
-import {View, TextInput} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, TextInput, FlatList} from 'react-native';
 import styles from './styles.js';
+import Entypo from 'react-native-vector-icons/Entypo';
 
-const DestinationSearchScreen = props => {
+import searchResults from '../../../assets/data/search';
+
+const DestinationSearchScreen = () => {
+  const [inputText, setInputText] = useState([]);
+
   return (
-    <View>
+    <View style={styles.container}>
       {/* Input Component */}
       <TextInput
         style={styles.textInput}
         placeholder="Where are you traveling?"
+        value={inputText}
+        onChangeText={setInputText}
       />
       {/* List of Destinations */}
+      <FlatList
+        data={searchResults}
+        renderItem={({item}) => (
+          <View style={styles.row}>
+            <View style={styles.iconContainer}>
+              <Entypo name={'location-pin'} size={30} />
+            </View>
+            <Text style={styles.locationText}>{item.description}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
