2606e70d2624f2a92ea4d03901a82e6186b8b80e
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-03-26 12:19:54 -0400

    Was able to install pods and import react native gesture handler, looking good

--- App.js
+++ App.js
@@ -5,18 +5,12 @@
  * @format
  * @flow strict-local
  */
-
+import 'react-native-gesture-handler';
 import React from 'react';
 import {SafeAreaView, StatusBar} from 'react-native';
 
 import HomeScreen from './src/screens/Home';
-import SearchResultsScreen from './src/screens/SearchResults';
-import DestinationSearchScreen from './src/screens/DestinationSearch';
-import GuestScreen from './src/screens/Guest';
-import Post from './src/components/Post';
-import feed from './assets/data/feed';
 
-const post1 = feed[0];
 const App: () => React$Node = () => {
   return (
     <>
